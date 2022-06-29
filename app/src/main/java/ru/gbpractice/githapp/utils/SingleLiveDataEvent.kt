be8f714678336f24f0eb23bec74f9cbd14e47046
Warcerg <warcerg@gmail.com>
2022-06-29 21:56:35 +0300

    добавлен SingleLiveDataEvent

--- app/src/main/java/ru/gbpractice/githapp/utils/SingleLiveDataEvent.kt
+++ app/src/main/java/ru/gbpractice/githapp/utils/SingleLiveDataEvent.kt
@@ -27,13 +27,7 @@ class SingleLiveDataEvent<T> : MutableLiveData<T>() {
         pending.set(true)
         super.setValue(t)
     }
-    /**
-     * Used for cases where T is Void, to make calls cleaner.
-     */
-    @MainThread
-    fun call() {
-        value = null
-    }
+
     companion object {
         private val TAG = "SingleLiveEvent"
     }
