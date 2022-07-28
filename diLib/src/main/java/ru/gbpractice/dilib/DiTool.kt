cc0cf14776d3d2a89f5df2ab8e1610b7fd1ac930
Warcerg <warcerg@gmail.com>
2022-07-28 13:31:50 +0300

    Добавил Qualifier

--- diLib/src/main/java/ru/gbpractice/dilib/DiTool.kt
+++ diLib/src/main/java/ru/gbpractice/dilib/DiTool.kt
@@ -4,26 +4,6 @@ import java.lang.IllegalArgumentException
 import kotlin.reflect.KClass
 
 object DiTool {
-/*    private val depMap = HashMap<KClass<*>, Any>()
-
-    fun <T : Any> add(clazz: KClass<T>, dependency: T) {
-        depMap[clazz] = dependency
-    }
-
-    inline fun <reified T : Any> add(dependency: T) {
-        add(T::class, dependency)
-    }
-
-    fun <T : Any> get(clazz: KClass<T>): T {
-        if (depMap.containsKey(clazz)) {
-            return depMap[clazz] as T
-        } else throw IllegalArgumentException("No dependency available to provide from map")
-    }
-
-    inline fun <reified T: Any> inject(): T {
-        return get(T::class)
-    }*/
-
     private val depMap = HashMap<Qualifier, Any>()
 
     fun <T : Any> add(qualifier: Qualifier, dependency: T) {
@@ -34,15 +14,15 @@ object DiTool {
         add(Qualifier(T::class), dependency)
     }
 
-    inline fun <reified T : Any> add(qualifierName: String,dependency: T) {
+    inline fun <reified T : Any> add(qualifierName: String, dependency: T) {
         add(Qualifier(T::class, qualifierName), dependency)
     }
 
-   data class Qualifier(
-       private val clazz: KClass<*>,
-       private val qualifierName: String = "Default"
+    data class Qualifier(
+        private val clazz: KClass<*>,
+        private val qualifierName: String = "Default"
 
-   )
+    )
 
     fun <T : Any> get(qualifier: Qualifier): T {
         if (depMap.containsKey(qualifier)) {
@@ -50,11 +30,11 @@ object DiTool {
         } else throw IllegalArgumentException("No dependency available to provide from map")
     }
 
-    inline fun <reified T: Any> inject(): T {
+    inline fun <reified T : Any> inject(): T {
         return get(Qualifier(T::class))
     }
 
-    inline fun <reified T: Any> inject(qualifierName: String): T {
+    inline fun <reified T : Any> inject(qualifierName: String): T {
         return get(Qualifier(T::class, qualifierName))
     }
 }
